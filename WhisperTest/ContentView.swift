789a12dbb6bd8314d92727c6afba16addb99a405
Andrew Zheng <aheze@getfind.app>
2024-02-17 02:56:58 -0800

    Initial Commit

---
+++ WhisperTest/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WhisperTest
+//  
+//  Created by Andrew Zheng (github.com/aheze) on 2/17/24.
+//  Copyright © 2024 Andrew Zheng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhisperTest/WhisperTestApp.swift
@@ -0,0 +1,18 @@
+//
+//  WhisperTestApp.swift
+//  WhisperTest
+//  
+//  Created by Andrew Zheng (github.com/aheze) on 2/17/24.
+//  Copyright © 2024 Andrew Zheng. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct WhisperTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
